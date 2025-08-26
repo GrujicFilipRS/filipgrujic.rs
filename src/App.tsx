6dd95e8b4310ae4488e5ab1e8340deb056ad90c7
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-08-26 20:39:55 +0200

    Fixed issue with Hero section and scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import './App.css'
 import Hero from './Hero.tsx'
 import Languages from './Languages.tsx'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const updateScrollbarWidth = () => {
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      document.documentElement.style.setProperty(
+        "--scrollbar-width",
+        `${scrollbarWidth}px`
+      );
+    };
+    updateScrollbarWidth();
+    window.addEventListener("resize", updateScrollbarWidth);
+    return () => window.removeEventListener("resize", updateScrollbarWidth);
+  }, []);
+
   return (
     <>
       <Hero/>
